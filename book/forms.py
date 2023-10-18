c9f6573f52040e2c64aec96f18177f14ed134ffe
user <murashkin_d@iuca.kg>
2023-10-18 17:27:25 +0600

    forms.py

--- book/forms.py
+++ book/forms.py
@@ -5,4 +5,4 @@ class ContactForm(forms.ModelForm):
 
     class Meta:
         model = Contact
-        fields = ('title', 'text',)
+        fields = ('name', 'family_name', 'surname', 'phone', 'email', 'photo', 'birth')
